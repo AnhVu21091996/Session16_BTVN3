c727c5837daac5d4e9c255d7e5214cfd5e39cf04
AnhVu21091996 <anhvuphan837@gmail.com>
2026-06-09 11:43:22 +0700

    BTVN3JS

---
+++ BTVN3.js
@@ -0,0 +1,10 @@
+function tinhTong(so1, so2) {
+  return so1 + so2;
+}
+
+alert('Tổng là: ' + tinhTong(5, 6));
+
+let num1 = Number(prompt('Nhập số 1: '));
+let num2 = Number(prompt('Nhập số 2: '));
+
+alert('Tổng hai số vừa nhập là: ' + tinhTong(num1, num2));
